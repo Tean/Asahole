87af1368e9380fc34eb90af864fb4dda994466b9
netteans <netteans@gmail.com>
2018-11-26 12:21:45 +0800

    split domain & service

--- cloud_modules/cloud_config_module/src/test/java/com/netteans/cloud/config/test/TestConfigBootStrap.java
+++ cloud_modules/cloud_config_module/src/test/java/com/netteans/cloud/config/test/TestConfigBootStrap.java
@@ -1,10 +1,13 @@
 package com.netteans.cloud.config.test;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringCloudApplication
 public class TestConfigBootStrap {
     public static void main(String[] args) {
-
+        SpringApplication.run(TestConfigBootStrap.class, args);
     }
 }
