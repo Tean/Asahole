53c06ea65fb86b2cdc644b22ae65ac78288baba6
netteans <netteans@gmail.com>
2019-03-18 01:00:47 +0800

    add vertx start @ main

--- vertx_example/src/main/java/com/netteans/vertx/example/starter/MainVerticle.java
+++ vertx_example/src/main/java/com/netteans/vertx/example/starter/MainVerticle.java
@@ -1,7 +1,7 @@
 package com.netteans.vertx.example.starter;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.*;
+import io.vertx.core.json.JsonObject;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -11,14 +11,23 @@ public class MainVerticle extends AbstractVerticle {
             req.response()
                     .putHeader("content-type", "text/plain")
                     .end("Hello from Vert.x!");
-        }).listen(8080, http -> {
+        }).listen(config().getInteger("http.port", 8080), http -> {
             if (http.succeeded()) {
                 startFuture.complete();
-                System.out.println("HTTP server started on http://localhost:8080");
+                System.out.println(String.format("HTTP server started on http://localhost:%d", 8081));
             } else {
                 startFuture.fail(http.cause());
             }
         });
+    }
+
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(
+                new MainVerticle(),
+                new DeploymentOptions().setConfig(new JsonObject().put("http.port", 8081)),
+                stringAsyncResult -> {
+                    System.out.println(stringAsyncResult.toString());
+                });
     }
 
 }
