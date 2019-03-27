c6cf4c604215840e412008d74e9427412d3daab3
netteans <netteans@gmail.com>
2019-03-27 16:46:57 +0800

    #examples

--- thrift_example/src/main/java/com/netteans/thrift/java/example/Bootstrap.java
+++ thrift_example/src/main/java/com/netteans/thrift/java/example/Bootstrap.java
@@ -1,20 +1,36 @@
 package com.netteans.thrift.java.example;
 
-import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootApplication
-public class Application {
+public class Bootstrap {
+
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 
     @Autowired
     private static ExampleConfigure exampleConfigure;
 
     public static void main(String[] args) {
+        logger.error("error test");
         System.out.println("cp is " + ClassLoader.getSystemResource(".").getPath());
+        try {
+            Files.list(Paths.get(ClassLoader.getSystemResource(".").getPath())).forEach(path -> {
+                System.out.println(path);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("port is: " + ExampleConfigure.getPort()); //获取不到配置文件的port
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Bootstrap.class, args);
         System.out.println("port is: " + ExampleConfigure.getPort()); //获取得到application.yml的port，需要spring-cloud-context才能获取到bootstrap.yml的port，如果application.yml有重复字段则会优先使用
     }
 }
