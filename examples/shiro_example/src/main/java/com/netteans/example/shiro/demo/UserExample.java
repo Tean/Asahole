d2ff6c0afbf03b57f84614eecba28bfb4673d78e
netteans <netteans@gmail.com>
2019-07-03 00:05:36 +0800

    Arraylist to concurrentMap

--- examples/shiro_example/src/main/java/com/netteans/example/shiro/demo/UserExample.java
+++ examples/shiro_example/src/main/java/com/netteans/example/shiro/demo/UserExample.java
@@ -2,37 +2,37 @@ package com.netteans.example.shiro.demo;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 public class UserExample {
-    private final List<User> userList = new ArrayList<>();
+    private final Map<String, User> userMap = new ConcurrentSkipListMap<String, User>();
 
     public UserExample() {
         User user = new User();
         user.setName("user");
         user.setPassword("1234");
         user.setRole("user");
-        userList.add(user);
+        userMap.put(user.getName(), user);
         User guster = new User();
         guster.setName("guester");
         guster.setPassword("1234");
         guster.setRole("guester");
-        userList.add(guster);
+        userMap.put(guster.getName(), guster);
         User admin = new User();
         admin.setName("admin");
         admin.setPassword("1234");
         admin.setRole("admin");
-        userList.add(admin);
+        userMap.put(admin.getName(), admin);
     }
 
     public User find(String username) {
         AtomicReference<User> user = new AtomicReference<>();
-        userList.forEach(u -> {
-            if (u.getName().equalsIgnoreCase(username))
-                user.set(u);
+        userMap.forEach((k, v) -> {
+            if (k.equalsIgnoreCase(username))
+                user.set(v);
         });
         return user.get();
     }
